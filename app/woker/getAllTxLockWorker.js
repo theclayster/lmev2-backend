8d4d1b80f395bdd356f6899dc856e4b9561f73d6
canhtuan97 <canhtuan97@gmail.com>
2020-12-22 11:59:47 +0700

    fix get all tx lock of address contract lock

--- app/woker/getAllTxLockWorker.js
+++ app/woker/getAllTxLockWorker.js
@@ -15,7 +15,9 @@ mongoose.connect(dbConfig.dbs, {
 });
 
 // address Lock
-ADDRESS_LOCK = ["0x17e00383a843a9922bca3b280c0ade9f8ba48449"]
+const ADDRESS_LOCK = ["0x17e00383a843a9922bca3b280c0ade9f8ba48449"]
+// bloock start
+const BLOCK_START = 0
 
 async function getAllTransaction(ADDRESS_LOCK) {
     for (let i = 0; i < ADDRESS_LOCK.length; i++) {
@@ -26,25 +28,31 @@ async function getAllTransaction(ADDRESS_LOCK) {
 
 async function insert_database(address_lock) {
     axios.get("https://api.etherscan.io/api?module=account&action=txlist&address=" +
-        address_lock + "&startblock=" + 10503172 + "&endblock=99999999&sort=asc&apikey=PJZTHQZRS1MTRUHTUHVHVYKBYX1RQVGPXP")
+        address_lock + "&startblock=" + BLOCK_START + "&endblock=99999999&sort=asc&apikey=PJZTHQZRS1MTRUHTUHVHVYKBYX1RQVGPXP")
         .then(async function (response) {
             data = response.data.result
             for (let i = 0; i < data.length; i++) {
 
                 try {
-                    console.log(data[i].blockNumber);
-                    inputData = data[i].input;
-                    decodeData = decoder.decodeData(inputData);
-                    let dataInsert = {
-                        "block_number": data[i].blockNumber,
-                        "from": data[i].from.toLowerCase(),
-                        "to": data[i].to.toLowerCase(),
-                        "hash": data[i].hash,
-                        "address_pool": "0x" + decodeData.inputs[0].toLowerCase(),
-                        "amount": parseInt(decodeData.inputs[1]),
-                        "unlock_date": parseInt(decodeData.inputs[2])
+                    check_exits_tx = await TransacionLockDB.findOne({ 'hash': data[i].hash })
+                    if (check_exits_tx) {
+                        console.log("tx " + data[i].hash + " already exist ");
+                    } else {
+                        console.log(data[i].blockNumber);
+                        inputData = data[i].input;
+                        decodeData = decoder.decodeData(inputData);
+                        let dataInsert = {
+                            "block_number": data[i].blockNumber,
+                            "from": data[i].from.toLowerCase(),
+                            "to": data[i].to.toLowerCase(),
+                            "hash": data[i].hash,
+                            "address_pool": "0x" + decodeData.inputs[0].toLowerCase(),
+                            "amount": parseInt(decodeData.inputs[1]),
+                            "unlock_date": parseInt(decodeData.inputs[2])
+                        }
+                        await new TransacionLockDB(dataInsert).save()
                     }
-                    await new TransacionLockDB(dataInsert).save()
+
                 } catch (error) {
                     continue
                 }
@@ -58,12 +66,8 @@ async function insert_database(address_lock) {
 
         })
         .catch(function (error) {
-            // handle error
             console.log(error);
         })
-        .then(function () {
-            // always executed
-        });
 }
 
 getAllTransaction(ADDRESS_LOCK)
